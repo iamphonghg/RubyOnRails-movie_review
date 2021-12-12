82d5eaea4ddae74d8a0d80fe0937d4fbb5f498ed
Phong Hoang Gia <gioxoay.xp@gmail.com>
2021-12-12 19:44:35 +0700

    done style index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,15 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  purge: [
-    "./app/**/*.html.erb"
-  ],
+  purge: ["./app/**/*.html.erb"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        cyan: colors.cyan,
-        'white': '#ffffff',
-        'purple': '#3f3cbb',
-        'midnight': '#121063',
-        'metal': '#565584',
-        'tahiti': '#3ab7bf',
-        'silver': '#evebff',
-        'bubble-gum': '#ff77e9',
-        'bermuda': '#78dcca',
-      }
-    }
+      fontFamily: {
+        'dosis': 'Dosis',
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 }
